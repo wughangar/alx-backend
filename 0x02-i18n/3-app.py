8169919c7abcfd58a47e38f804fe654aa193ed9d
wughangar <wughangar.rose@gmail.com>
2024-01-10 09:00:31 +0000

    parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+2. Get locale from request
+"""
+from flask import Flask, render_template
+from flask_babel import Babel, get_locale
+from flask import request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """function to render template"""
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ function that gets the locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
