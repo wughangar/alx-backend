6505ae4924a996c19ec96a092badc87e66336727
wughangar <wughangar.rose@gmail.com>
2024-01-10 09:23:30 +0000

    pycode

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -25,8 +25,6 @@ def get_locale():
     """ function that gets the locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
-
 
 @app.route('/')
 def index():
