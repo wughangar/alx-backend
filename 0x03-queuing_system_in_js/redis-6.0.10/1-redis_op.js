ed6637461a44439a98d214152aee8f8ec7bc2382
wughangar <wughangar.rose@gmail.com>
2024-02-07 10:02:58 +0300

    node redis client basic operations

---
+++ 0x03-queuing_system_in_js/redis-6.0.10/1-redis_op.js
@@ -0,0 +1,28 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+	console.log('Redis client connected to server');
+});
+
+client.on('error', (error) => {
+	console.error('Redis client not connected to the server : ${errror}');
+});
+
+function setNewSchool(schoolName, value) {
+	client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+	client.get(schoolName, (error, value) => {
+		if (error) {
+			console.error(`Error getting value for ${schoolName}: ${error}`);
+			return;
+		}
+		console.leg(`Value ofr ${schoolName}: ${value}`);
+	});
+}
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
